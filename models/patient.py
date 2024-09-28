b2423b8b788ba7a765cf25ea1cfb264749b3198c
MH Pourkhaje <mh.pourkhaje@a.com>
2024-09-28 11:52:11 +0330

    om_hospital-1

--- models/patient.py
+++ models/patient.py
@@ -5,5 +5,6 @@ class Hospitalpatient(models.Model):
     _description = "hospital patient"
 
     name = fields.Char(string="Name")
+    ref = fields.Char(string="Refrence")
     age = fields.Integer(string="Age")
     gender = fields.Selection(string="Gender", selection=[('male', 'Male'), ('Female', 'female')])
