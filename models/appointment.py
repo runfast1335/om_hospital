b2077bee6bb590904a1803b3d6fa1252f8e2463a
MH Pourkhaje <mh.pourkhaje@a.com>
2024-10-12 17:05:01 +0330

    vid-26-rec_name

--- models/appointment.py
+++ models/appointment.py
@@ -1,10 +1,20 @@
-from odoo import models, fields
+from odoo import models, fields, api
+
 
 class HospitalAppooiment(models.Model):
     _name = "hospital.appooiment"
     _description = "hospital Appooiment"
     _inherit = "mail.thread", "mail.activity.mixin"
+    _rec_name = "ref"
 
     patient_id = fields.Many2one("hospital.patient", string="Patient",)
+    gender = fields.Selection(related="patient_id.gender",) #readonly=False
     appooiment_time = fields.Datetime(string = "appooiment time", default=fields.Date.today())
     booking_date = fields.Date(string = "booking date", default=fields.Date.today())
+    ref = fields.Char(string="Refrence",readonly=True)
+
+
+
+    @api.onchange("patient_id")
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
--- models/patient.py
+++ models/patient.py
@@ -1,13 +1,29 @@
+from datetime import date
 from odoo import models, fields
 
 class Hospitalpatient(models.Model):
     _name = "hospital.patient"
     _description = "hospital patient"
     _inherit = "mail.thread", "mail.activity.mixin"
+    _rec_name = "name"
 
     # tracking=True به درد لاگ انداختن توی چتر میخوره
     name = fields.Char(string="Name", tracking=True)
-    ref = fields.Char(string="Refrence", tracking=True)
-    age = fields.Integer(string="Age", tracking=True)
-    gender = fields.Selection(string="Gender", selection=[('male', 'Male'), ('female', 'Female')], required=True, tracking=True)
-    active = fields.Boolean(string="Active", default=True, tracking=True)
+    date_of_birth = fields.Date(string="Date of birth")
+    ref = fields.Char(string="Refrence")
+    age = fields.Integer(string="Age",compute="_compute_age", tracking=True)
+    gender = fields.Selection(string="Gender", selection=[('male', 'Male'), ('female', 'Female')], required=True
+                              , tracking=True, default="male")
+    active = fields.Boolean(string="Active", default=True)
+    appointment_id = fields.Many2one("hospital.appooiment", string="Appooiment", )
+
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            print("today", today)
+            if rec.date_of_birth:
+               print("rec.date_of_birth", rec.date_of_birth, rec.date_of_birth.year)
+               rec.age = today.year - rec.date_of_birth.year
+            else:
+               rec.age = 0
+
