d31d612e8710e650c1347c761bda5b733776b850
MH Pourkhaje <mh.pourkhaje@a.com>
2024-12-05 00:33:19 +0330

    vid-30-priority

--- models/appointment.py
+++ models/appointment.py
@@ -7,13 +7,17 @@ class HospitalAppooiment(models.Model):
     _inherit = "mail.thread", "mail.activity.mixin"
     _rec_name = "ref"
 
-    patient_id = fields.Many2one("hospital.patient", string="Patient",)
-    gender = fields.Selection(related="patient_id.gender",) #readonly=False
-    appooiment_time = fields.Datetime(string = "appooiment time", default=fields.Date.today())
-    booking_date = fields.Date(string = "booking date", default=fields.Date.today())
-    ref = fields.Char(string="Refrence",readonly=True)
-
-
+    patient_id = fields.Many2one("hospital.patient", string="Patient", )
+    gender = fields.Selection(related="patient_id.gender", )  # readonly=False
+    appooiment_time = fields.Datetime(string="appooiment time", default=fields.Date.today())
+    booking_date = fields.Date(string="booking date", default=fields.Date.today())
+    ref = fields.Char(string="Refrence", readonly=True)
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string="Priority",
+        help='Gives the sequence order when displaying a list of MRP documents.')
 
     @api.onchange("patient_id")
     def onchange_patient_id(self):
