f237e4bdfd8f294225acacc6430bbcf7b7f0ae08
MH Pourkhaje <mh.pourkhaje@a.com>
2024-12-05 00:59:13 +0330

    vid-30-priority

--- models/patient.py
+++ models/patient.py
@@ -1,6 +1,7 @@
 from datetime import date
 from odoo import models, fields
 
+
 class Hospitalpatient(models.Model):
     _name = "hospital.patient"
     _description = "hospital patient"
@@ -11,19 +12,19 @@ class Hospitalpatient(models.Model):
     name = fields.Char(string="Name", tracking=True)
     date_of_birth = fields.Date(string="Date of birth")
     ref = fields.Char(string="Refrence")
-    age = fields.Integer(string="Age",compute="_compute_age", tracking=True)
-    gender = fields.Selection(string="Gender", selection=[('male', 'Male'), ('female', 'Female')], required=True
-                              , tracking=True, default="male")
+    age = fields.Integer(string="Age", compute="_compute_age", tracking=True)
+    gender = fields.Selection(string="Gender", selection=[('male', 'Male'), ('female', 'Female')],
+                              required=True, tracking=True, default="male")
     active = fields.Boolean(string="Active", default=True)
-    appointment_id = fields.Many2one("hospital.appooiment", string="Appooiment", )
+    appointment_id = fields.Many2one("hospital.appooiment",
+                                     string="Appooiment")
 
     def _compute_age(self):
         for rec in self:
             today = date.today()
             print("today", today)
             if rec.date_of_birth:
-               print("rec.date_of_birth", rec.date_of_birth, rec.date_of_birth.year)
-               rec.age = today.year - rec.date_of_birth.year
+                print("rec.date_of_birth", rec.date_of_birth, rec.date_of_birth.year)
+                rec.age = today.year - rec.date_of_birth.year
             else:
-               rec.age = 0
-
+                rec.age = 0
